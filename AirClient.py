e728d66952718cfdec8001cb1f4b86d6c795122d
boke1208 <boke1208@gmail.com>
2018-05-23 14:11:59 +0800

    客户端完成

---
+++ AirClient.py
@@ -0,0 +1,124 @@
+#-*- coding:utf-8 -*-
+import socket
+import time
+import re, ConfigParser
+
+class AirClient(object):
+    #初始化
+    def __init__(self, room=503, currentTemp=15, finalTemp=25, wind=2):
+        self.room = room
+        self.mode = 'hot'
+        self.currentTemp = currentTemp
+        self.finalTemp = finalTemp
+        self.wind = wind
+        self.totalMoney = 0.0
+        self.startTime = int(time.time())
+        self.lastTime = int(time.time())
+        self.sleep = False
+        self.open = True
+        self.status_to_money()
+        self.is_sleep()
+
+   def init(self, room=503, currentTemp=15, finalTemp=25, wind=2):
+        self.room = room
+        self.mode = 'hot'
+        self.currentTemp = currentTemp
+        self.finalTemp = finalTemp
+        self.wind = wind
+        self.totalMoney = 0.0
+        self.startTime = int(time.time())
+        self.lastTime = int(time.time())
+        self.sleep = False
+        self.open = True
+        self.status_to_money()
+        self.is_sleep()
+
+    def send_start(self):
+        #{:0>2d} 左边补0
+        sendBuf = 'start_{room}_$'.format({'room':self.room})
+        return sendBuf
+
+    def send_first_open(self):
+        sendBuf = 'r_{room}_{currentTemp}_{finalTemp}_{wind}_$'
+        sendBuf = sendBuf.format({'room':self.room,
+                                    'currentTemp':self.currentTemp,
+                                    'finalTemp':self.finalTemp,
+                                    'wind':self.wind})
+        return sendBuf
+
+    def send_open(self):
+        sendBuf = 'r_{room}_{currentTemp}_{finalTemp}_{wind}_$'
+        sendBuf = sendBuf.format({'room':self.room,
+                                    'currentTemp':self.currentTemp,
+                                    'finalTemp':self.finalTemp,
+                                    'wind':self.wind})
+        return sendBuf
+
+    def send_change(self):
+        sendBuf = 'c_{room}_{currentTemp}_{finalTemp}_{wind}_$'
+        sendBuf = sendBuf.format({'room':self.room,
+                                    'currentTemp':self.currentTemp,
+                                    'finalTemp':self.finalTemp,
+                                    'wind':self.wind})
+        return sendBuf
+
+    def send_close(self):
+        sendBuf = 'close_{room}_$'
+        sendBuf = sendBuf.format({'room':self.room})
+        return sendBuf
+
+    def recv_start(self, operate):
+        status = {}
+        status['room'] = operate[1]
+        if operate[2] == '1':
+            status['mode'] = 'hot'
+        else:
+            status['mode'] = 'cold'
+        status['tempFrom'] = operate[3].split('-')[0]
+        status['tempTo'] = operate[3].split('-')[1]
+        status['tempWidth'] = operate[4]
+
+        self.change_status(status)
+
+    def recv_a(self,operate):
+        status = {}
+        status['room'] = operate[1]
+        status['currentTemp'] = operate[2]
+        status['totalMoney'] = operate[3]
+        status['time'] = operate[4]
+        status['finalTemp'] = operate[5]
+        status['wind'] = operate[6]
+        status['tempChange'] = operate[7]
+        status['preMoney'] = operate[8]
+        status['totalElec'] = operate[9]
+
+        self.change_status(status)
+
+    def recv_close(self, operate):
+        if operate[2] == '1':
+            print('退房了!')
+            return False
+        self.open = True
+        return True
+
+    def recv_sleep(self, operate):
+        self.sleep = True
+
+    def recv_wait(self, operate):
+        print 'i recv a wait message!'
+
+    #改变状态
+    def change_status(self, **kwargs):
+        if 'room' in kwargs:
+            self.room = kwargs['room']
+        if 'currentTemp' in kwargs:
+            self.currentTemp = kwargs['currentTemp']
+        if 'finalTemp' in kwargs:
+            self.finalTemp = kwargs['finalTemp']
+        if 'wind' in kwargs:
+            self.wind = kwargs['wind']
+
+    #test：展示状态
+    def show_status(self):
+        #print 'room:', self.room, 'currentTemp:', self.currentTemp, 'finalTemp:', self.finalTemp, 'wind:', self.wind
+        print dir(self)
--- client.py
+++ client.py
@@ -1,13 +1,10 @@
 #-*- coding:utf-8 -*-
 import socket
 import time
-from Air import *
+from AirClient import *
 # Socket Init
 HOST, PORT = "127.0.0.1", int(2333)
-room = 503
-currentTemp = 15
-finalTemp = 25
-wind = 2
+cold = 0.1
 
 if __name__ == '__main__':
     # Connect to Server
@@ -16,31 +13,45 @@ if __name__ == '__main__':
     time.sleep(0.2)
 
     air = Air()
+    status={'room':'503',
+            'currentTemp':20.3,
+            'finalTemp':28.0,
+            'wind':2,#中速
+            }
+    air.change_status(status)
     air.show_status()    
-    sendBuf = air.send_status()
+    sendBuf = air.send_first_open()
     print sendBuf
     sock.send(sendBuf)
-    
+    time.sleep(0.2)
 
     opStr = ''
     while 1:
         res = sock.recv(1024)
         opStr += res
         operate = opStr.split("_")
-
-        if operate[0] == 'a' and len(operate) == 10:
+        if operate[0] == 'start' and len(operate) == 7 and operate[-1] == '$':
+            air.recv_start(operate)
             opStr = ''
             print operate
-        if operate[0] == 'close' and len(operate) == 2:
+        if operate[0] == 'a' and len(operate) == 11 and operate[-1] == '$':
+            air.recv_a(operate)
             opStr = ''
             print operate
-        if operate[0] == 'sleep' and len(operate) == 2:
+        if operate[0] == 'close' and len(operate) == 4 and operate[-1] == '$':
+            air.recv_close(operate)
             opStr = ''
+            print operate
+        if operate[0] == 'sleep' and len(operate) == 3 and operate[-1] == '$':
+            air.recv_sleep(operate)
+            opStr = ''
+            print operate
             #待机
-        if operate[0] == 'wait' and len(operate) == 3:
+        if operate[0] == 'wait' and len(operate) == 5 and operate[-1] == '$':
+            air.recv_wait(operate)
             opStr = ''
-            #啥是等待？
-
-
+            #阻塞，等待服务
+        air.show_status()
+            
         time.sleep(0.1)
     sock.close()
